5e756641c6418e3b6c5b4f456c3c9e55693ee46c
PRAKASH GANGURDE <gangurdeprakash220@gmail.com>
2025-11-23 03:42:58 +0530

    first commit

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCfkrltgIAYr2osTLxYpqwMRcb5VvAuA0c",
+  authDomain: "vault-3f547.firebaseapp.com",
+  projectId: "vault-3f547",
+  storageBucket: "vault-3f547.firebasestorage.app",
+  messagingSenderId: "808570214698",
+  appId: "1:808570214698:web:50295014999efae0fd2653",
+  measurementId: "G-Z60FB0YSMF"
+};
